a246ead6d6f681e47cf0f9fe3c3539d70fc0b02c
reynoldadade <reynoldadade@gmail.com>
2022-06-24 22:14:46 +0000

    setup search input

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const location = ref({});
 const error = ref(false);
-// const place = ref("");
+const place = ref("");
 
 // emitters
 const emit = defineEmits({
@@ -34,6 +34,50 @@ function searchPlace(place) {
       }
     });
 }
+
+function searchWithLonLat(lat, lon) {
+  axios
+    .get(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=ae11ff30d19df8f21cb53a7b12688d1d&limit=1`
+    )
+    .then((response) => {
+      const [first] = response.data;
+      place.value = first.name;
+    });
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        searchWithLonLat(latitude, longitude);
+      },
+      (error) => {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+        }
+      }
+    );
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+onMounted(() => {
+  getLocation();
+});
 
 // function initAutocomplete() {
 //   autocomplete({
@@ -81,7 +125,7 @@ function searchPlace(place) {
     id="places"
     v-model="place"
     autocomplete="on"
-    class="border border-red-500 p-2 rounded-lg"
+    class="border border-gray-200 p-2 rounded-lg focus:shadow-lg"
     @keydown.enter="searchPlace(place)"
   />
   <!-- </div> -->
