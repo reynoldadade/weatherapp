17ee07fb42f8c81fab4ff865b0222c66ef424734
reynoldadade <reynoldadade@gmail.com>
2022-06-24 17:48:58 +0000

    create api functions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
 <script setup>
+import { ref, onMounted, computed } from "vue";
+import axios from "axios";
+
+const currentWeatherData = ref({});
+const historicWeatherData = ref({});
+
+// request for current weather data
+function getCurrentWeatherData(lat, lon) {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=ae11ff30d19df8f21cb53a7b12688d1d&units=metric&exclude=hourly`
+    )
+    .then((response) => {
+      currentWeatherData.value = response.data;
+    });
+}
+
+function createHistoricalWeatherDataRequest(lat, lon) {
+  const timeStampRequests = [];
+  // last 5 days as timestamps
+  for (let i = 5; i > 0; i--) {
+    const today = Date.now();
+    const days = Math.floor((today - 24 * 60 * 60 * 1000 * i) / 1000);
+    const request = axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&appid=ae11ff30d19df8f21cb53a7b12688d1d&units=metric&exclude=hourly&dt=${days}&only_current=true`
+    );
+    timeStampRequests.push(request);
+  }
+
+  return timeStampRequests;
+}
+
+function makeHistoricalWeatherDataRequest(lat, lon) {
+  Promise.all(createHistoricalWeatherDataRequest(lat, lon)).then(
+    (responses) => {
+      historicWeatherData.value = responses.map((response) => response.data);
+    }
+  );
+}
+
+onMounted(() => {
+  getCurrentWeatherData(5.614818, -0.205874);
+  makeHistoricalWeatherDataRequest(5.614818, -0.205874);
+  // console.log(createHistoricalWeatherDataRequest(5.614818, -0.205874));
+});
+
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>hello</div>
+  <div>{{ historyLinks }}</div>
 </template>
 
 <style>
